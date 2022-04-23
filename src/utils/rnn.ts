3439a0be952545f5ab792eb874f1f395e74a56d4
cow.moo <cow.moo.code@gmail.com>
2022-04-23 04:44:45 +0200

    rnn - remove nonnumeridal items

---
+++ src/utils/rnn.ts
@@ -0,0 +1,15 @@
+/**
+ * rnn - remove nonnumerical items
+ * @param {Array<any>} items - [-5, "abc", 3, 0]
+ * @returns {number} - returns [-5, 3, 0]
+ */
+
+export const rnn = (items: any[]): number[] => {
+    items = items.filter(item => !isNaN(item)); // remove non-numeric items
+
+    if (!items.length) {
+        return []; // return undefined
+    }
+
+    return items;
+};
