c149b0002ef65aaf0f5b2341026a337a4ba3c909
cow.moo <cow.moo.code@gmail.com>
2022-04-23 06:27:08 +0200

    updated sort.ts

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -17,7 +17,7 @@ export const sorta = (items: number[]): number[] => {
  * @param {Array<number>} items - [2, 3, 1]
  * @returns {Array<number>} - returns [3, 2, 1]
  */
-export const sortd = (items: any[]): number | number[] => {
+export const sortd = (items: number[]): number | number[] => {
 
     if (!items.length) {
         return []; // return undefined
