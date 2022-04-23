c423a39c2b1787bc522d6fa7e0fa3477595b57e7
cow.moo <cow.moo.code@gmail.com>
2022-04-23 04:49:30 +0200

    comment spacing

--- src/utils/rnn.ts
+++ src/utils/rnn.ts
@@ -3,7 +3,6 @@
  * @param {Array<any>} items - [-5, "abc", 3, 0]
  * @returns {number} - returns [-5, 3, 0]
  */
-
 export const rnn = (items: any[]): number[] => {
     items = items.filter(item => !isNaN(item)); // remove non-numeric items
 
